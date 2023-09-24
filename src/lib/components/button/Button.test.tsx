3291f96165d302e0b16f6f7ec0ecce312f280f1e
Wael Rassaa <wael@it-labs.fr>
2023-09-24 21:26:53 +0100

    fix package name

--- src/lib/components/button/Button.test.tsx
+++ src/lib/components/button/Button.test.tsx
@@ -4,7 +4,7 @@ import { Button } from './index'
 describe('Button',()=>{
     it('should render',async () => {
         const label = 'test button'
-        render(<Button label={label} />)
+        render(<Button children={<span>{label}</span>} />)
         expect(screen.getByText(label)).toBeDefined()
     })
 })
