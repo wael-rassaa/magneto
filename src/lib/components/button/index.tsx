d5adc8866cd709e71939f602d84a78140a5d8264
Wael Rassaa <wael@it-labs.fr>
2023-09-25 16:33:02 +0100

    button-fix

--- src/lib/components/button/index.tsx
+++ src/lib/components/button/index.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
         variant: {
           default: "bg-slate-900 text-white hover:bg-slate-900/80",
           destructive:
-            "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+            "bg-red-500 text-white hover:bg-red-500/80",
           outline:
             "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
           secondary:
